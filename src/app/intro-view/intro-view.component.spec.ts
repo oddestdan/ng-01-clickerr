62bbef8de8e5c337635035d3c088f7e91630ec4b
Dan <oddestdan@gmail.com>
2020-04-22 14:32:44 +0300

    Fix tests due to updated data logic and routing

--- src/app/intro-view/intro-view.component.spec.ts
+++ src/app/intro-view/intro-view.component.spec.ts
@@ -18,8 +18,4 @@ describe('IntroViewComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
--- src/app/result-view/result-view.component.spec.ts
+++ src/app/result-view/result-view.component.spec.ts
@@ -18,8 +18,4 @@ describe('ResultViewComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
